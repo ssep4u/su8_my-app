804761ce21039b97b04812a799baea63899a51d8
ssep4u <ssep4u@gmail.com>
2019-11-27 17:11:13 +0900

    Mission Complete!🤫

--- src/index.js
+++ src/index.js
@@ -66,13 +66,13 @@ class Game extends React.Component {
     super(props);
     this.state = {
       history: [  {  squares: Array(9).fill(null),  }  ],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber+1);
     const current = this.state.history[history.length-1];
-    
     // if (this.state.squares[i] == null) {
       const squares = current.squares.slice(); //원본 squares 보존하자
       if(calculateWinner(squares) || squares[i] != null) {
@@ -81,19 +81,26 @@ class Game extends React.Component {
       squares[i] = this.state.xIsNext ? 'X' : 'O';
       this.setState({
         history: history.concat(  [  {squares: squares,}  ]  ),
+        stepNumber: history.length,
         xIsNext: !this.state.xIsNext,
       });
     // }
   }
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step%2)===0,
+    });
+  }
   render() {
     const history = this.state.history;
-    const current = history[history.length-1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
       const desc = move?'Go to move #'+move:'Go to game start';
       return (
-        <li>
+        <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       );
